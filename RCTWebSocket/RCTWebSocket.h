414052e850c09b228327f870e9b02a7c3fa76656
Tyler Thompson <tyler@tylerpaulthompson.com>
2015-04-02 16:54:28 -0400

    Initial Commit

---
+++ RCTWebSocket/RCTWebSocket.h
@@ -0,0 +1,19 @@
+//
+//  RCTWebSocket.h
+//  RCTWebSocket
+//
+//  Created by Admin on 4/2/15.
+//  Copyright (c) 2015 tptee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RCTWebSocket.
+FOUNDATION_EXPORT double RCTWebSocketVersionNumber;
+
+//! Project version string for RCTWebSocket.
+FOUNDATION_EXPORT const unsigned char RCTWebSocketVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RCTWebSocket/PublicHeader.h>
+
+
